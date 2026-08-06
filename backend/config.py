4e48c443b6ac5676b37cb9e1da6fd722e64968cd
unknown <maheshtdl782018>
2026-08-06 17:06:10 +0530

    Connected Railway MySQL

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,17 @@
-class Config:
-    SECRET_KEY = "intelai_secret_key"
 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Welcome@localhost/intelai_db"
+import os
+
+class Config:
+
+    SECRET_KEY = os.getenv("SECRET_KEY", "intelai_secret_key")
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{os.getenv('MYSQLUSER')}:"
+        f"{os.getenv('MYSQLPASSWORD')}@"
+        f"{os.getenv('MYSQLHOST')}:"
+        f"{os.getenv('MYSQLPORT')}/"
+        f"{os.getenv('MYSQLDATABASE')}"
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
